c024f0e2e47172a3ba74f9e3d6aceeb1c353b607
Spencer Tuft <spencer.tuft@gmail.com>
2019-09-11 16:39:22 -0600

    Adding more flexibility

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,23 @@ import Edge from './edge';
 import Vertex from './vertex';
 
 export default class Graph<T> {
-    public vertices: {[key: string]: Vertex<T>} = {};
-    public edges: {[key: string]: Edge<T>} = {};
+    public vertices: { [key: string]: Vertex<T> } = {};
+    public edges: { [key: string]: Edge<T> } = {};
     public isDirected = false;
 
     constructor(isDirected: boolean) {
         this.isDirected = isDirected;
     }
 
-    public addVertex(value: Vertex<T>) {
-        this.vertices[value.getKey()] = value;
-        return this;
+    public addVertex(value: T | Vertex<T>, key?: string) {
+        let vertex: Vertex<T>;
+        if (!(value instanceof Vertex)) {
+            vertex = new Vertex(value, key);
+        } else {
+            vertex = value;
+        }
+        this.vertices[vertex.getKey()] = vertex;
+        return vertex;
     }
 
     public getVertexByKey(vertexKey: string) {
@@ -31,38 +37,40 @@ export default class Graph<T> {
         return Object.values(this.edges);
     }
 
-    public addEdge(edge: Edge<T>) {
+    public addEdge(start: Vertex<T>, end: Vertex<T>) {
+        const value = new Edge<T>(start, end);
+
         // Try to find and end start vertices.
-        let startVertex = this.getVertexByKey(edge.start.getKey());
-        let endVertex = this.getVertexByKey(edge.end.getKey());
+        let startVertex = this.getVertexByKey(value.start.getKey());
+        let endVertex = this.getVertexByKey(value.end.getKey());
 
         // Insert start vertex if it wasn't inserted.
         if (!startVertex) {
-            this.addVertex(edge.start);
-            startVertex = this.getVertexByKey(edge.start.getKey());
+            this.addVertex(value.start);
+            startVertex = this.getVertexByKey(value.start.getKey());
         }
 
         // Insert end vertex if it wasn't inserted.
         if (!endVertex) {
-            this.addVertex(edge.end);
-            endVertex = this.getVertexByKey(edge.end.getKey());
+            this.addVertex(value.end);
+            endVertex = this.getVertexByKey(value.end.getKey());
         }
 
         // Check if edge has been already added.
-        if (this.edges[edge.getKey()]) {
+        if (this.edges[value.getKey()]) {
             throw new Error('Edge has already been added before');
         } else {
-            this.edges[edge.getKey()] = edge;
+            this.edges[value.getKey()] = value;
         }
 
         // Add edge to the vertices.
         if (this.isDirected) {
             // If graph IS directed then add the edge only to start vertex.
-            startVertex.add(edge);
+            startVertex.add(value);
         } else {
             // If graph ISN'T directed then add the edge to both vertices.
-            startVertex.add(edge);
-            endVertex.add(edge);
+            startVertex.add(value);
+            endVertex.add(value);
         }
 
         return this;
@@ -109,14 +117,14 @@ export default class Graph<T> {
             edge.reverse();
 
             // Add reversed edge back to the graph and its vertices.
-            this.addEdge(edge);
+            this.addEdge(edge.start, edge.end);
         });
 
         return this;
     }
 
     public getVerticesIndices() {
-        const verticesIndices: {[key: string]: number} = {};
+        const verticesIndices: { [key: string]: number } = {};
         this.getAllVertices().forEach((vertex, index) => {
             verticesIndices[vertex.getKey()] = index;
         });
